8d0c513f86aceb8f3704d3a22a9e47ec23ce2049
GirdharSharma <girdhar0702@gmail.com>
2026-01-09 21:37:22 +0530

    sitemap update

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,4 +1,16 @@
+const axios = require("axios");
 module.exports = {
   siteUrl: "https://thenexusindia.in",
   generateRobotsTxt: true,
+
+  additionalPaths: async () => {
+    const res = await axios.get(
+      "https://coral-rail-888634.hostingersite.com/blog"
+    );
+
+    return res.data.data.map((blog) => ({
+      loc: `/blogs/${blog.id}/${blog.title.toLowerCase().replace(/ /g, "-")}`,
+      lastmod: new Date(blog.updated_at || blog.created_at).toISOString(),
+    }));
+  },
 };
